cbc9c4dbf9f67db6dbfb016c0ae974aa86dcddcc
Gabriel Girouard <admin@bungeetaco.com>
2020-02-19 21:56:07 -0500

    Update envsetup script location

--- templates/build_template.go
+++ templates/build_template.go
@@ -838,13 +838,8 @@ build_aosp() {
   ############################
   # from original setup.sh script
   ############################
-  source build/envsetup.sh
-  export LANG=C
-  export _JAVA_OPTIONS=-XX:-UsePerfData
-  export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date --utc +%Y.%m.%d.%H)
+  source script/envsetup.sh
   log "BUILD_NUMBER=$BUILD_NUMBER"
-  export DISPLAY_BUILD_NUMBER=true
-  chrt -b -p 0 $$
 
   choosecombo $BUILD_TARGET
   log "Running target-files-package"
