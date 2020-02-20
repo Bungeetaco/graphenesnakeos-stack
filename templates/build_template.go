1c35b3752bc83609751e70e11f63f8422e967fbd
Bungeetaco <admin@bungeetaco.com>
2020-02-20 01:34:58 -0500

    Update release.sh script to use GrapheneOS env script

--- templates/build_template.go
+++ templates/build_template.go
@@ -845,13 +845,8 @@ release() {
   ############################
   # from original setup.sh script
   ############################
-  source build/envsetup.sh
-  export LANG=C
-  export _JAVA_OPTIONS=-XX:-UsePerfData
-  export BUILD_NUMBER=$(cat out/build_number.txt 2>/dev/null || date --utc +%Y.%m.%d.%H)
+  source script/envsetup.sh
   log "BUILD_NUMBER=$BUILD_NUMBER"
-  export DISPLAY_BUILD_NUMBER=true
-  chrt -b -p 0 $$
 
   ############################
   # from original release.sh script
